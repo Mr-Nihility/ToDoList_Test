b79aed39fba964825d759b98e4081a37e6c4b203
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2024-04-28 16:54:48 +0300

    add navlink to home on errorView

--- src/views/ErrorView/ErrorView.tsx
+++ src/views/ErrorView/ErrorView.tsx
@@ -1,8 +1,13 @@
+import { NavLink } from "react-router-dom";
+import { RouterPath } from "../../types/router.types";
+
 export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+
+      <NavLink to={RouterPath.Home}>Go Home</NavLink>
     </div>
   );
 }
