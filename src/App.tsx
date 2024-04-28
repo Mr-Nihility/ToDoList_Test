073aa64cd7c5f672fe3545d7471b5c70ef3a9868
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2024-04-28 16:32:47 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route
-          path={"/ToDoList_Test" + RouterPath.Home}
-          element={<NavigationLayout />}
-        >
+        <Route path={"/ToDoList_Test/"} element={<NavigationLayout />}>
           <Route
             index
             element={
